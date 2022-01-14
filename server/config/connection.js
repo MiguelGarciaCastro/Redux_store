a98f9e9c5fa4bc42cd4ee298551d8001614e4054
Miguel Garcia-Castro <garciamiguel7700@yahoo.com>
2022-01-14 17:43:50 -0500

    got the server working

---
+++ server/config/connection.js
@@ -0,0 +1,13 @@
+// bring in .env
+require('dotenv').config();
+
+const mongoose = require('mongoose');
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/mernshopping', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+module.exports = mongoose.connection;
